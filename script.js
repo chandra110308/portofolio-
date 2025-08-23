5d836bf23a7bcc510125d81485115c33d681a858
Chandra <putrachandra496@gmail.com>
2025-08-23 20:08:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Init AOS
 AOS.init({
   duration: 1000,
-  mirror+once: true
+  mirror: true
 });
 
 // Typed.js
@@ -9,5 +9,7 @@ var typed = new Typed('#typed', {
   strings: ["Cyber Security", "Network Engineer", "IT Support"],
   typeSpeed: 70,
   backSpeed: 50,
-  loop: true
+  loop: true,
+  showCursor: true,   // biar hanya 1 cursor
+  cursorChar: "|"     // kursor custom (bisa diganti misal "_" atau kosong "")
 });
